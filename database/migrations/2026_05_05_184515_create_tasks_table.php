cc4ea3498aebe47b24d14acbc2478c7e18a08781
Devula123 <devxd8231@gmail.com>
2026-05-05 21:01:23 +0200

    Dodanie modelu i migracji

--- database/migrations/2026_05_05_184515_create_tasks_table.php
+++ database/migrations/2026_05_05_184515_create_tasks_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('is_completed')->default(false);
+
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
